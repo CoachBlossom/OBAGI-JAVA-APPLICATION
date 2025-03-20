669f809e86e88bc47484f667dba563de62a456f6
CoachBlossom <blossomjumbo38@gmail.com>
2025-03-20 14:29:02 +0100

    UserInput

---
+++ TypeCasting.java
@@ -0,0 +1,19 @@
+public class TypeCasting{
+      public static void main(String[] args){
+	       double balance = 58585;
+		   System.out.printf("Your account balance is %c%f%n",'$',balance);
+		   
+		   //Narrowing TypeCasting (ASCII code ? = 63)
+		   char alpha = '?';
+		   
+		   int asciiCode = (char)alpha;
+		   System.out.printf("The ASCII code for ? is %d%n", asciiCode);
+		   
+		   
+		   double height =5.8;
+		   short myHeight =(short)height;
+		   System.out.printf("My Faculty's height is %d%n",myHeight);
+		   
+		   
+	}
+}	
